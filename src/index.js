80175f37fb9ae27972e9ba281d90bdc0f24db661
Hugo Javadi <hugoj4vadi@gmail.com>
2021-03-22 09:10:51 +0000

    feat: connect to mongo

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
+const mongoose = require('mongoose');
 const app = require('./app');
 
 const port = 3000;
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
-});
+async function start() {
+  try {
+    await mongoose.connect('mongodb://localhost/test', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to mongo');
+  } catch (e) {
+    console.error(e);
+  }
+
+  app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+  });
+}
+start();
