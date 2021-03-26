38e7cfbfe1d41e6b454781a0369b7ef6f3abd3ff
Hugo Javadi <hugoj4vadi@gmail.com>
2021-03-26 10:12:49 +0000

    refactor: delete mongoose connection code

--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
-const mongoose = require('mongoose');
 const app = require('./app');
 
 const port = 3000;
 
 async function start() {
-  try {
-    await mongoose.connect('mongodb://localhost/test', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('Connected to mongo');
-  } catch (e) {
-    console.error(e);
-  }
-
   app.listen(port, () => {
     console.log(`App listening on port ${port}`);
   });
