57533526827ad252cbecc56bd51e8f85248ecaf6
Hugo Javadi <hugoj4vadi@gmail.com>
2021-03-27 17:05:53 +0000

    feat: connect to mongo

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
+const { MongoClient } = require('mongodb');
 const app = require('./app');
 
-const port = 3000;
+async function connectToMongo() {
+  const client = new MongoClient('mongodb://localhost/test', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 
-async function start() {
+  try {
+    await client.connect();
+    console.log('Connected to mongo');
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+function listen() {
+  const port = 3000;
+
   app.listen(port, () => {
     console.log(`App listening on port ${port}`);
   });
 }
+
+async function start() {
+  await connectToMongo();
+  listen();
+}
+
 start();
