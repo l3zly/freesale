5e83e9fade8ae6c65d2644bd961f32919a7717d7
Hugo Javadi <hugoj4vadi@gmail.com>
2021-04-03 20:42:59 +0100

    feat: add signup route

--- src/user/user-api.js
+++ src/user/user-api.js
@@ -1,5 +1,15 @@
 const express = require('express');
+const { signup } = require('./user-handler');
 
 const router = express.Router();
+
+router.post('/', async (req, res, next) => {
+  try {
+    const user = await signup({ body: req.body });
+    res.status(201).json(user);
+  } catch (e) {
+    next(e);
+  }
+});
 
 module.exports = router;
