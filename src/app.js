65fac63a4358d3a20a8d7bf54dcfb736e326e909
Hugo Javadi <hugoj4vadi@gmail.com>
2021-04-03 20:39:47 +0100

    feat: add errors middleware

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const { userApi } = require('./user');
+const { errors } = require('./middleware');
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/users', userApi);
+
+app.use(errors);
 
 module.exports = app;
