7889c08a9122e5c369f5d28f8817515ee62189f7
Hugo Javadi <hugoj4vadi@gmail.com>
2021-04-03 20:18:44 +0100

    feat: add module interface

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const users = require('./user');
+const { userApi } = require('./user');
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/users', users);
+app.use('/users', userApi);
 
 module.exports = app;
--- src/user/index.js
+++ src/user/index.js
@@ -1,5 +1,5 @@
-const express = require('express');
+const userApi = require('./user-api');
 
-const router = express.Router();
-
-module.exports = router;
+module.exports = {
+  userApi,
+};
