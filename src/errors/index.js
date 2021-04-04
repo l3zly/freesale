39165101a07a4b447e7e0254ca5e5c89ab5ad0b3
Hugo Javadi <hugoj4vadi@gmail.com>
2021-04-04 15:36:50 +0100

    feat: add not found error

--- src/errors/index.js
+++ src/errors/index.js
@@ -1,7 +1,9 @@
 const HttpError = require('./http-error');
 const BadRequestError = require('./bad-request-error');
+const NotFoundError = require('./not-found-error');
 
 module.exports = {
   HttpError,
   BadRequestError,
+  NotFoundError,
 };
