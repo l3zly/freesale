4fee6914ad6d9548d78a8480c65a560850113d91
Hugo Javadi <hugoj4vadi@gmail.com>
2021-04-04 11:26:56 +0100

    feat: remove password from result

--- src/user/user-dal.js
+++ src/user/user-dal.js
@@ -2,6 +2,7 @@ const { client } = require('../db');
 
 async function save(user) {
   const result = await client.db().collection('users').insertOne(user);
+  delete result.ops[0].password;
   return result.ops[0];
 }
 
