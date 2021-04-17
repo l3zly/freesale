40b4d1409d785d7a7d5c3d13bae93569714726b8
Hugo Javadi <hugoj4vadi@gmail.com>
2021-04-17 18:39:58 +0100

    feat: add decodeToken function

--- src/services/token-service.js
+++ src/services/token-service.js
@@ -3,11 +3,17 @@ const jwt = require('jsonwebtoken');
 const { tokenSecret } = require('../config');
 
 const sign = promisify(jwt.sign);
+const verify = promisify(jwt.verify);
 
 async function generateToken(payload) {
   return await sign(payload, tokenSecret);
 }
 
+async function decodeToken(token) {
+  return await verify(token, tokenSecret);
+}
+
 module.exports = {
   generateToken,
+  decodeToken,
 };
--- src/services/token-service.test.js
+++ src/services/token-service.test.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const { generateToken } = require('./token-service');
+const { generateToken, decodeToken } = require('./token-service');
 
 process.env.TOKEN_SECRET = 'shhhhh';
 
@@ -11,6 +11,17 @@ describe('tokenService', () => {
       const token = await generateToken(payload);
 
       const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
+
+      expect(decoded.foo).toEqual(payload.foo);
+    });
+  });
+  describe('decodeToken', () => {
+    it('decodes a jwt', async () => {
+      const payload = { foo: 'bar' };
+
+      const token = await generateToken(payload);
+
+      const decoded = await decodeToken(token);
 
       expect(decoded.foo).toEqual(payload.foo);
     });
