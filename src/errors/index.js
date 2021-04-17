9943fdb31b556d46371bc2f478f5349de9cbaacd
Hugo Javadi <hugoj4vadi@gmail.com>
2021-04-17 19:34:47 +0100

    feat: export unauthorized error

--- src/errors/index.js
+++ src/errors/index.js
@@ -1,9 +1,11 @@
 const HttpError = require('./http-error');
 const BadRequestError = require('./bad-request-error');
 const NotFoundError = require('./not-found-error');
+const UnauthorizedError = require('./unauthorized-error');
 
 module.exports = {
   HttpError,
   BadRequestError,
   NotFoundError,
+  UnauthorizedError,
 };
