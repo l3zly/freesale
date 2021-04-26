8b7e009fc5f6d5687d87df53fcc67da58ce95957
Hugo Javadi <hugoj4vadi@gmail.com>
2021-04-26 20:45:39 +0100

    feat: add geo service

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,9 @@
 const passwordService = require('./password-service');
 const tokenService = require('./token-service');
+const geoService = require('./geo-service');
 
 module.exports = {
   passwordService,
   tokenService,
+  geoService,
 };
