db0111c785331d8d7f1a354f57e11924e18217d4
Hugo Javadi <hugoj4vadi@gmail.com>
2021-05-01 13:01:34 +0100

    feat: only allow uk phone numbers

--- src/user/user-schema.js
+++ src/user/user-schema.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 
 const userSchema = Joi.object({
   phone: Joi.string()
-    .pattern(/^\+[1-9]\d{1,14}$/)
+    .pattern(/^\+44\d{1,14}$/)
     .required(),
   password: Joi.string().min(8).required(),
   postcode: Joi.string()
