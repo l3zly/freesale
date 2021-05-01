8f70349c11d84329265550bed8df7f57924ceb03
Hugo Javadi <hugoj4vadi@gmail.com>
2021-05-01 12:56:54 +0100

    feat: only allow uk postcodes

--- src/user/user-schema.js
+++ src/user/user-schema.js
@@ -5,7 +5,11 @@ const userSchema = Joi.object({
     .pattern(/^\+[1-9]\d{1,14}$/)
     .required(),
   password: Joi.string().min(8).required(),
-  postcode: Joi.string().required(),
+  postcode: Joi.string()
+    .pattern(
+      /^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([AZa-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z])))) [0-9][A-Za-z]{2})$/
+    )
+    .required(),
   coords: Joi.object({
     lon: Joi.number().required(),
     lat: Joi.number().required(),
