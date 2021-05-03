e601568baa0f7826cd89507d7b14faf444460cdd
Hugo Javadi <hugoj4vadi@gmail.com>
2021-05-03 13:59:48 +0100

    feat: use unified topology

--- src/upload.js
+++ src/upload.js
@@ -2,7 +2,12 @@ const GridFsStorage = require('multer-gridfs-storage');
 const multer = require('multer');
 const { mongoUri: url } = require('./config');
 
-const storage = new GridFsStorage({ url });
+const storage = new GridFsStorage({
+  url,
+  options: {
+    useUnifiedTopology: true,
+  },
+});
 
 const upload = multer({ storage });
 
