2e9960f8704c4324ae555637b43a1e696244ee61
Hugo Javadi <hugoj4vadi@gmail.com>
2021-05-03 14:02:13 +0100

    feat: add imageIds key

--- src/offer/offer-schema.js
+++ src/offer/offer-schema.js
@@ -4,6 +4,7 @@ const offerSchema = Joi.object({
   title: Joi.string().required(),
   amount: Joi.string().required(),
   status: Joi.string().valid('accepted', 'declined', 'pending').required(),
+  imageIds: Joi.array().items(Joi.string()).required(),
   requestId: Joi.string(),
   userId: Joi.string().required(),
 });
