5193c4bd81d7ac5951ce509f69d94d48149cfd48
Hugo Javadi <hugoj4vadi@gmail.com>
2021-05-08 19:12:33 +0100

    feat: add security config

---
+++ src/main/java/io/freesale/security/Config.java
@@ -0,0 +1,63 @@
+package io.freesale.security;
+
+import io.freesale.repository.UserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.ReactiveAuthenticationManager;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
+import org.springframework.security.web.server.authentication.ServerAuthenticationConverter;
+
+@EnableWebFluxSecurity
+public class Config {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http,
+                                                         ReactiveAuthenticationManager authenticationManager,
+                                                         ServerAuthenticationConverter serverAuthenticationConverter) {
+        var authenticationWebFilter = new AuthenticationWebFilter(authenticationManager);
+        authenticationWebFilter.setServerAuthenticationConverter(serverAuthenticationConverter);
+        return http
+                .csrf()
+                .disable()
+                .httpBasic()
+                .disable()
+                .formLogin()
+                .disable()
+                .logout()
+                .disable()
+                .authorizeExchange()
+                .anyExchange()
+                .authenticated()
+                .and()
+                .addFilterAt(authenticationWebFilter, SecurityWebFiltersOrder.AUTHENTICATION)
+                .build();
+    }
+
+    @Bean
+    public ServerAuthenticationConverter serverAuthenticationConverter() {
+        return new TokenAuthenticationConverter();
+    }
+
+    @Bean
+    public ReactiveAuthenticationManager authenticationManager(TokenUtil tokenUtil,
+                                                               ReactiveUserDetailsService userDetailsService) {
+        return new TokenAuthenticationManager(tokenUtil, userDetailsService);
+    }
+
+    @Bean
+    public ReactiveUserDetailsService userDetailsService(UserRepository userRepository) {
+        return new MongoUserDetailsService(userRepository);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
