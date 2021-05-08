a910556be873ac9757134d1f7de7cc6967b52d13
Hugo Javadi <hugoj4vadi@gmail.com>
2021-05-08 19:05:19 +0100

    refactor: delete redundant method

--- src/main/java/io/freesale/repository/UserRepository.java
+++ src/main/java/io/freesale/repository/UserRepository.java
@@ -2,10 +2,7 @@ package io.freesale.repository;
 
 import io.freesale.model.User;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import reactor.core.publisher.Mono;
 
 public interface UserRepository extends ReactiveMongoRepository<User, String> {
-
-    Mono<User> findUserByPhone(String phone);
 
 }
