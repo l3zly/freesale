841a854646e59bac26846d27c176f9aa465e4a7d
Hugo Javadi <hugoj4vadi@gmail.com>
2021-05-13 22:05:30 +0100

    feat: add token dto

---
+++ src/main/java/io/freesale/dto/TokenDto.java
@@ -0,0 +1,21 @@
+package io.freesale.dto;
+
+public class TokenDto {
+
+    private final String accessToken;
+    private final String tokenType;
+
+    public TokenDto(String accessToken, String tokenType) {
+        this.accessToken = accessToken;
+        this.tokenType = tokenType;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public String getTokenType() {
+        return tokenType;
+    }
+
+}
