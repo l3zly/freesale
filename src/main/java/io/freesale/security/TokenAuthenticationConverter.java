a01c6da1314539f581c46bb8eb93e56b4fc276ad
Hugo Javadi <hugoj4vadi@gmail.com>
2021-05-13 23:09:16 +0100

    feat: add extractTokenFromAuthorizations method

--- src/main/java/io/freesale/security/TokenAuthenticationConverter.java
+++ src/main/java/io/freesale/security/TokenAuthenticationConverter.java
@@ -1,11 +1,14 @@
 package io.freesale.security;
 
 import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 import org.springframework.security.web.server.authentication.ServerAuthenticationConverter;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 public class TokenAuthenticationConverter implements ServerAuthenticationConverter {
 
@@ -13,9 +16,22 @@ public class TokenAuthenticationConverter implements ServerAuthenticationConvert
     public Mono<Authentication> convert(ServerWebExchange serverWebExchange) {
         return Mono
                 .just(serverWebExchange.getRequest().getHeaders().getOrEmpty(HttpHeaders.AUTHORIZATION))
-                .filter(authorizations -> !authorizations.isEmpty())
-                .map(authorizations -> authorizations.get(0).split("Bearer ")[1])
+                .map(this::extractTokenFromAuthorizations)
                 .map(token -> new PreAuthenticatedAuthenticationToken(null, token));
+    }
+
+    private String extractTokenFromAuthorizations(List<String> authorizations) {
+        if (authorizations.isEmpty()) {
+            throw new AuthenticationCredentialsNotFoundException("Authorization header is empty");
+        }
+
+        var tokenTypeAndToken = authorizations.get(0).split(" ");
+
+        if (tokenTypeAndToken.length < 2) {
+            throw new AuthenticationCredentialsNotFoundException("Authorization is formatted incorrectly");
+        }
+
+        return tokenTypeAndToken[1];
     }
 
 }
