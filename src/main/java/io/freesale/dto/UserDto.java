d3018f3dd45400cc8a4b30e173fac63a991f1eeb
Hugo Javadi <hugoj4vadi@gmail.com>
2021-05-13 23:12:59 +0100

    feat: add user dto

---
+++ src/main/java/io/freesale/dto/UserDto.java
@@ -0,0 +1,21 @@
+package io.freesale.dto;
+
+public class UserDto {
+
+    private final String id;
+    private final String phone;
+
+    public UserDto(String id, String phone) {
+        this.id = id;
+        this.phone = phone;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+}
