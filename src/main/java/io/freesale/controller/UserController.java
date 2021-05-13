3e368c7f0a4f6595157d33bfd64232e93c9770ab
Hugo Javadi <hugoj4vadi@gmail.com>
2021-05-13 23:13:47 +0100

    feat: add get me route

--- src/main/java/io/freesale/controller/UserController.java
+++ src/main/java/io/freesale/controller/UserController.java
@@ -2,8 +2,11 @@ package io.freesale.controller;
 
 import io.freesale.dto.CreateUserDto;
 import io.freesale.dto.TokenDto;
+import io.freesale.dto.UserDto;
+import io.freesale.security.SecurityUser;
 import io.freesale.service.UserService;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
@@ -21,6 +24,13 @@ public class UserController {
     @ResponseStatus(HttpStatus.CREATED)
     public Mono<TokenDto> signup(@RequestBody Mono<CreateUserDto> createUserDto) {
         return userService.signup(createUserDto);
+    }
+
+    @GetMapping("/me")
+    public Mono<UserDto> getMe(Mono<Authentication> authentication) {
+        return authentication
+                .map(auth -> (SecurityUser) auth.getPrincipal())
+                .map(securityUser -> new UserDto(securityUser.getUser().getId(), securityUser.getUser().getPhone()));
     }
 
 }
