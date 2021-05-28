b727ad85bcd6ee5ba07bd3923ac35e5630a66e42
Hugo Javadi <hugoj4vadi@gmail.com>
2021-05-28 21:57:27 +0100

    feat: add make request dto

---
+++ src/main/java/io/freesale/dto/MakeRequestDto.java
@@ -0,0 +1,15 @@
+package io.freesale.dto;
+
+public class MakeRequestDto {
+
+    private final String title;
+
+    public MakeRequestDto(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+}
