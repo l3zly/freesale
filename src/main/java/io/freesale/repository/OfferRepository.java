2eac3978cd8190c43cf91017b988fd75be207362
Hugo Javadi <hugoj4vadi@gmail.com>
2021-05-28 23:48:20 +0100

    feat: add offer repository

---
+++ src/main/java/io/freesale/repository/OfferRepository.java
@@ -0,0 +1,7 @@
+package io.freesale.repository;
+
+import io.freesale.model.Offer;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+public interface OfferRepository extends ReactiveMongoRepository<Offer, String> {
+}
