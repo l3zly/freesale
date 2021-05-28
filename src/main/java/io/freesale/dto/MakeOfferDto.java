f11cdd861042068b4394e18aa4a425faaa09c2f0
Hugo Javadi <hugoj4vadi@gmail.com>
2021-05-28 23:55:03 +0100

    feat: add make offer dto

---
+++ src/main/java/io/freesale/dto/MakeOfferDto.java
@@ -0,0 +1,15 @@
+package io.freesale.dto;
+
+public class MakeOfferDto {
+
+    private final String amount;
+
+    public MakeOfferDto(String amount) {
+        this.amount = amount;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+}
