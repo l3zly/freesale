56fa38f07b43d78a26e50887b4616a1548d67731
Hugo Javadi <hugoj4vadi@gmail.com>
2021-05-30 23:38:26 +0100

    feat: add image urls field

--- src/main/java/io/freesale/model/Offer.java
+++ src/main/java/io/freesale/model/Offer.java
@@ -2,25 +2,30 @@ package io.freesale.model;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.Collections;
+import java.util.List;
+
 @Document(collection = "offers")
 public class Offer {
 
     private final String id;
     private final String amount;
     private final Status status;
+    private final List<String> imageUrls;
     private final String requestId;
     private final String userId;
 
-    public Offer(String id, String amount, Status status, String requestId, String userId) {
+    public Offer(String id, String amount, Status status, List<String> imageUrls, String requestId, String userId) {
         this.id = id;
         this.amount = amount;
         this.status = status;
+        this.imageUrls = imageUrls;
         this.requestId = requestId;
         this.userId = userId;
     }
 
     public static Offer of(String amount, Status status, String requestId, String userId) {
-        return new Offer(null, amount, status, requestId, userId);
+        return new Offer(null, amount, status, Collections.emptyList(), requestId, userId);
     }
 
     public String getId() {
@@ -33,6 +38,10 @@ public class Offer {
 
     public Status getStatus() {
         return status;
+    }
+
+    public List<String> getImageUrls() {
+        return imageUrls;
     }
 
     public String getRequestId() {
