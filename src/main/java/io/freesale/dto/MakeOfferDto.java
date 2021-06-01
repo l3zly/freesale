78f9664ce64de72b7d277a17b283c2d2170e0b2f
Hugo Javadi <hugoj4vadi@gmail.com>
2021-06-01 19:55:20 +0100

    fix: map amount to json property

--- src/main/java/io/freesale/dto/MakeOfferDto.java
+++ src/main/java/io/freesale/dto/MakeOfferDto.java
@@ -1,10 +1,12 @@
 package io.freesale.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class MakeOfferDto {
 
     private final String amount;
 
-    public MakeOfferDto(String amount) {
+    public MakeOfferDto(@JsonProperty("amount") String amount) {
         this.amount = amount;
     }
 
