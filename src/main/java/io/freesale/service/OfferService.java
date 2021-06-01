f34c6ab8f6170a7dd461dd64c2a848e05ae05d8b
Hugo Javadi <hugoj4vadi@gmail.com>
2021-06-01 20:02:06 +0100

    feat: add offer service

---
+++ src/main/java/io/freesale/service/OfferService.java
@@ -0,0 +1,24 @@
+package io.freesale.service;
+
+import io.freesale.dto.MakeOfferDto;
+import io.freesale.model.Offer;
+import io.freesale.repository.OfferRepository;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+public class OfferService {
+
+    private final OfferRepository repository;
+
+    public OfferService(OfferRepository repository) {
+        this.repository = repository;
+    }
+
+    public Mono<Offer> makeOffer(Mono<MakeOfferDto> makeOfferDto, String userId) {
+        return makeOfferDto
+                .map(dto -> Offer.of(dto.getAmount(), Offer.Status.PENDING, null, userId))
+                .flatMap(repository::save);
+    }
+
+}
