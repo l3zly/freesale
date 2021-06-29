732548a03a85ce3c03e99d56ee3cab641855954c
Hugo Javadi <hugoj4vadi@gmail.com>
2021-06-29 08:09:52 +0100

    feat: view open offers
    Closes #7

--- src/main/java/io/freesale/controller/OfferController.java
+++ src/main/java/io/freesale/controller/OfferController.java
@@ -6,11 +6,13 @@ import io.freesale.security.SecurityUser;
 import io.freesale.service.OfferService;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -29,6 +31,11 @@ public class OfferController {
       Authentication authentication) {
     return offerService
         .makeOffer(makeOfferDto, ((SecurityUser) authentication.getPrincipal()).getUser().getId());
+  }
+
+  @GetMapping
+  public Flux<Offer> getOpenOffers() {
+    return offerService.findOpenOffers();
   }
 
 }
--- src/main/java/io/freesale/service/OfferService.java
+++ src/main/java/io/freesale/service/OfferService.java
@@ -4,6 +4,7 @@ import io.freesale.dto.MakeOfferDto;
 import io.freesale.model.Offer;
 import io.freesale.repository.OfferRepository;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
@@ -19,6 +20,10 @@ public class OfferService {
     return makeOfferDto
         .map(dto -> Offer.of(dto.getAmount(), Offer.Status.PENDING, null, userId))
         .flatMap(repository::save);
+  }
+
+  public Flux<Offer> findOpenOffers() {
+    return repository.findByRequestIdIsNull();
   }
 
 }
