7c64ea7c7cee507cfa59811720ffee617aae702c
Hugo Javadi <hugoj4vadi@gmail.com>
2021-06-30 16:48:50 +0100

    fix: map title to json property

--- src/main/java/io/freesale/dto/MakeRequestDto.java
+++ src/main/java/io/freesale/dto/MakeRequestDto.java
@@ -1,10 +1,12 @@
 package io.freesale.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class MakeRequestDto {
 
   private final String title;
 
-  public MakeRequestDto(String title) {
+  public MakeRequestDto(@JsonProperty("title") String title) {
     this.title = title;
   }
 
