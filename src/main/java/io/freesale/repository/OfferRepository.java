d3fb34cb95f92be417fd7b28b75d49ed69a8bc6e
Hugo Javadi <hugoj4vadi@gmail.com>
2021-07-02 16:41:37 +0100

    refactor: delete query method

--- src/main/java/io/freesale/repository/OfferRepository.java
+++ src/main/java/io/freesale/repository/OfferRepository.java
@@ -2,10 +2,7 @@ package io.freesale.repository;
 
 import io.freesale.model.Offer;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import reactor.core.publisher.Flux;
 
 public interface OfferRepository extends ReactiveMongoRepository<Offer, String> {
-
-  Flux<Offer> findByRequestIdIsNull();
 
 }
