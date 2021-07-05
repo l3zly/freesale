b332b3f9159cdf5c9ca9117af990102fe3fd85e9
Hugo Javadi <hugoj4vadi@gmail.com>
2021-07-05 21:36:06 +0100

    feat: add find offer by id and request id query

--- src/main/java/io/freesale/repository/OfferRepository.java
+++ src/main/java/io/freesale/repository/OfferRepository.java
@@ -3,9 +3,12 @@ package io.freesale.repository;
 import io.freesale.model.Offer;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface OfferRepository extends ReactiveMongoRepository<Offer, String> {
 
   Flux<Offer> findByRequestId(String requestId);
+
+  Mono<Offer> findByIdAndRequestId(String id, String requestId);
 
 }
