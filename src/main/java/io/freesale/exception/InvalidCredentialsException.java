91ed6df18d2855d1b7b871ec78b86434c4afbedf
Hugo Javadi <hugoj4vadi@gmail.com>
2021-07-05 21:22:37 +0100

    feat: add invalid credentials exception

---
+++ src/main/java/io/freesale/exception/InvalidCredentialsException.java
@@ -0,0 +1,9 @@
+package io.freesale.exception;
+
+public class InvalidCredentialsException extends RuntimeException {
+
+  public InvalidCredentialsException() {
+    super("Invalid credentials");
+  }
+
+}
